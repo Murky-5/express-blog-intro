9ac7db2bf613ae1342358830e9d75ce525490524
MirkoDD <mirkodidio111@gmail.com>
2025-01-20 17:45:43 +0100

    added:posts(js)_list_of_posts,bridged_to_index(js)_and_displayed_at_local_bacheca

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /*
 
-Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
-Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
-Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post.
-Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
-Testare su postman
+Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog” ✔
+Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe) ✔
+Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post. ✔
+Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post. ✔
+Testare su postman ✔
 
 */
 
@@ -17,15 +17,15 @@ const port = 3000
 // ```js```
 const express = require('express')
 const app = express()
-
+const memePosts = require('./posts.js')
 
 // starts a simple http server locally on port 3000
 app.get('', (req, res) => {
-    res.send("cioa")
+    res.send("Server del mio blog")
 });
 
-app.get('/info', (req, res) => {
-    res.send("100cc")
+app.get('/bacheca', (req, res) => {
+    res.send(memePosts)
 })
 
 app.listen(port, `${hostname}`, () => {
